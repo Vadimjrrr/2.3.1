6548737a0ed6ff6d10b0b3ca8e4ad48a17733c2f
Вадим <Vadyok95@mail.ru>
2022-04-17 21:21:24 +0500

    Initial commit

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,17 @@
+package dao;
+
+import model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+
+    void delete(Long id);
+
+    void update(User user);
+
+    List<User> getList();
+
+    User getUser(Long id);
+}
